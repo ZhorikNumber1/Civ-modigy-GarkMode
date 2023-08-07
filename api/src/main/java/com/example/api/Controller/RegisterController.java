c4b6066291e50bddc605f95aaa45c540f5d0e62e
Pavel <109911382+ZhorikNumber1@users.noreply.github.com>
2023-08-07 15:57:44 +0300

    Update RegisterController.java

--- api/src/main/java/com/example/api/Controller/RegisterController.java
+++ api/src/main/java/com/example/api/Controller/RegisterController.java
@@ -3,6 +3,7 @@ package com.example.api.Controller;
 import com.example.api.Model.User;
 import com.example.api.Repository.userRepository;
 import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/security")
 public class RegisterController {
+    @Autowired
     private userRepository UserRepository;
     @PostMapping("/register")
-    public JSONPObject newUser(@RequestBody User user){
+    public boolean newUser(@RequestBody User user){
         UserRepository.save(user);
-
+        return true;
     }
 }
